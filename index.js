a98bb61f7000ff6eec48a14fa69fc0dd574aa436
Gil <gcastillohtx@gmail.com>
2021-11-30 10:04:30 -0600

    commit tree 846f818ab56e25de7b81026f580f18b0c48f9fa2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+const findMAtching = drivers.filter(function(value) {
+    return value === ['bobby', 'sally'];
+}); 
+
+console.log(drivers);
+
+
+
+  
